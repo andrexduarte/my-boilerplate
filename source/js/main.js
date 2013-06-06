dc6407511bf444e6b1fa66167f75a978905a3f6f
Andre Duarte <andrexduarte@gmail.com>
2013-06-06 11:32:46 -0300

    First commit

---
+++ source/js/main.js
@@ -0,0 +1,6 @@
+require(["jquery", "jquery.plugins", "jquery.beta"], function($) {
+    //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
+    $(function() {
+        $('body').plugins().beta();
+    });
+});
